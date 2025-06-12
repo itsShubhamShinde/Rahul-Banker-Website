7c922c25f65c2131ef969b8abbd40afbc83da7e4
itsShubhamShinde <itsshubhamks@gmail.com>
2025-06-12 06:26:07 +0530

    Add vercel.json for proper routing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// 👇 This is the correct code for Vercel deployment
 export default defineConfig({
   plugins: [react()],
-})
+});
